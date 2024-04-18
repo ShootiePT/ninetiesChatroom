66d4c878b988f6e386610d63a824d1593ec5f988
RafaelM <rafaelbecamagalhaes@hotmail.com>
2024-04-18 06:17:47 +0100

    6 am login design

---
+++ js/login.js
@@ -0,0 +1,11 @@
+import { Input, Ripple, initTWE } from "tw-elements";
+
+
+// Initialization for ES Users
+import {
+    Input,
+    Ripple,
+    initTWE,
+  } from "tw-elements";
+  
+  initTWE({ Input, Ripple });
