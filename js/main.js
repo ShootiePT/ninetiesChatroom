048a894c03b1c124a6bb26f89b8f9d856fdff83d
RafaelM <rafaelbecamagalhaes@hotmail.com>
2024-04-18 11:35:00 +0100

    11:40 am ainda mais frito, chatroom update

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const button = document.getElementById('user-menu-button');
+const menu = document.querySelector('#profile-dropdown > div:last-child');
+function toggleMenu() {
+    menu.classList.toggle('hidden');
+}
+button.addEventListener('click', toggleMenu);
+document.addEventListener('click', function(event) {
+    const isClickInsideMenu = menu.contains(event.target);
+    const isClickInsideButton = button.contains(event.target);
+    if (!isClickInsideMenu && !isClickInsideButton) {
+        menu.classList.add('hidden');
+    }
+});
---
+++ js/menu.js
@@ -0,0 +1,7 @@
+const mobileMenuToggle = document.getElementById('mobile-menu-toggle');
+    const mobileMenu = document.getElementById('mobile-menu');
+    mobileMenuToggle.addEventListener('click', function () {
+        const isExpanded = mobileMenuToggle.getAttribute('aria-expanded') === 'false';
+        mobileMenuToggle.setAttribute('aria-expanded', !isExpanded);
+        mobileMenu.classList.toggle('hidden');
+    });
